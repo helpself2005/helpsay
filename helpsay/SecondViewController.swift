ad420ce4a6ddedafadb6888e45480f5926700f4b
shipl <277940154@qq.com>
2015-09-14 10:37:19 +0800

    first add

---
+++ helpsay/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  helpsay
+//
+//  Created by shipl on 15/9/14.
+//  Copyright (c) 2015年 shipl. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
